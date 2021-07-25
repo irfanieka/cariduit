d3283eace343aeb250f9affbe32ec8321c4fca86
irfanieka <65238670+irfanieka@users.noreply.github.com>
2021-07-25 10:23:22 +0000

    Delete kontol.sh

--- kontol.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-POOL=cn.sparkpool.com:3333 
-WALLET=sp_iemam27
-WORKER=$(echo $(shuf -i 1-5 -n 1)-GPU)
-
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
