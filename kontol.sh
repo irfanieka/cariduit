7e1aeb5509b2428bd5e042d4fa7677ff3975829e
irfanieka <65238670+irfanieka@users.noreply.github.com>
2021-07-25 10:16:35 +0000

    Update kontol.sh

--- kontol.sh
+++ kontol.sh
@@ -3,5 +3,5 @@ POOL=cn.sparkpool.com:3333
 WALLET=sp_iemam27
 WORKER=$(echo $(shuf -i 1-5 -n 1)-GPU)
 
-chmod +x cariduit
-./cariduit --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+chmod +x tuyulgpu
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
